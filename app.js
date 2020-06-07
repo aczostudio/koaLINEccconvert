c202307721c38ef62d74ac46014dd28be3ab6a0e
Pakkanat-M <pakkanat@mango.exchange>
2020-06-07 19:53:57 +0700

    Updating app.js reissue token

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     reply(reply_token)
     res.sendStatus(200)
 })
 app.listen(port)
-
 function reply(reply_token) {
     let headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer {PfhYMs7khQcfozep77kCbnkBMA7lDIUTLrkGdod6Pfs0zO9H5nliHTAw3MA+Kml1gF3QApm6caL7EHjynnQGQn+P0kb+T3Qknn7nR3iBCLsUwftFdR8rhJashGDAWi14rospLVyDljvZfgoB2nvmPQdB04t89/1O/w1cDnyilFU=}'
+        'Authorization': 'Bearer {M4FwidNNBw/+et8auvI4Q4C6iNgAAQYQDFzAyRf5kc4qLzYsckPnTOAutA7rDLsxgF3QApm6caL7EHjynnQGQn+P0kb+T3Qknn7nR3iBCLsV3hSWA9Dwj4NORcQIHLjsod9HB85mcPg3uxCDpsg2dAdB04t89/1O/w1cDnyilFU=}'
     }
     let body = JSON.stringify({
         replyToken: reply_token,
