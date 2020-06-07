d9267896da2c2652948c502f11084bb04d32f696
Pakkanat-M <pakkanat@mango.exchange>
2020-06-07 18:21:10 +0700

    Updating app.js distDir

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
+var distDir = __dirname+ "/dist/"
+app.use(express.static(distDir))
 app.post('/webhook', (req, res) => res.sendStatus(200))
 app.listen(port)
