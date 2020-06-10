af0f0796565de772fb97a89512b34158e846ab92
Pakkanat-M <pakkanat@mango.exchange>
2020-06-10 23:35:44 +0700

    fix and add catch error

--- app.js
+++ app.js
@@ -34,30 +34,34 @@ router
         ctx.body = 'Hello World!';
     })
     .post('/webhook', async (ctx, next) => {
-        let reply_Token = ctx.request.body.events[0].replyToken;
-        if(reply_Token ===  '00000000000000000000000000000000') {
-            ctx.status = 200;
-        } else {
-            await router.post({
-                method: 'POST',
-                url: 'https://api.line.me/v2/bot/message/reply',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer {ShkuSBy9o1OAXcyOyYYBctPrd3aY3ZuUdDlTfdvjomurWcnE38CjKechkXF++7G9wh8C6/1Ise1iEVQzcwHpr1nwsajzUieMqrZZmgtga8ZNmbKfech4BV0zui3kkzYpqUu752d4Uzol+gPpXvJcpwdB04t89/1O/w1cDnyilFU=}'
-                },
-                body: ({
-                    replyToken: reply_token,
-                    messages: [{
-                        type: 'text',
-                        text: 'Hello'
+        try {
+            let reply_Token = ctx.request.body.events[0].replyToken;
+            if(reply_Token ===  '00000000000000000000000000000000') {
+                ctx.status = 200;
+            } else {
+                await router.post({
+                    method: 'POST',
+                    url: 'https://api.line.me/v2/bot/message/reply',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer {ShkuSBy9o1OAXcyOyYYBctPrd3aY3ZuUdDlTfdvjomurWcnE38CjKechkXF++7G9wh8C6/1Ise1iEVQzcwHpr1nwsajzUieMqrZZmgtga8ZNmbKfech4BV0zui3kkzYpqUu752d4Uzol+gPpXvJcpwdB04t89/1O/w1cDnyilFU=}'
                     },
-                    {
-                        type: 'text',
-                        text: 'How are you?'
-                    }]
-                }),
-                json: true
-            })
+                    body: ({
+                        replyToken: reply_token,
+                        messages: [{
+                            type: 'text',
+                            text: 'Hello'
+                        },
+                        {
+                            type: 'text',
+                            text: 'How are you?'
+                        }]
+                    }),
+                    json: true
+                })
+            }
+        } catch (err) {
+            console.log('server error', err, ctx);
         }
     });
 
